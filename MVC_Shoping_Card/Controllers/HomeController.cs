05df30860580fc26ce4a47904c4be823cf47ec4e
Soul_To_Sell <hr2breach@gmail.com>
2025-09-23 00:31:16 +0330

    Fixed Initial Commit

---
+++ MVC_Shoping_Card/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MVC_Shoping_Card.Models;
+
+namespace MVC_Shoping_Card.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVC_Shoping_Card/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_Shoping_Card.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
