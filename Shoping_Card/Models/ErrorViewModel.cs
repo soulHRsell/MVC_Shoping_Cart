4986f18c3e172f6fc698c7932a6d7b2fdee31cad
Soul_To_Sell <hr2breach@gmail.com>
2025-09-22 19:34:20 +0330

    First Commit

---
+++ Shoping_Card/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Shoping_Card.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
